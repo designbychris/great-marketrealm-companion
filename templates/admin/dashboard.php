de45f4d29b9b1d07a390835a2a50d1cfbf729948
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-15 16:48:34 +0100

    Create dashboard.php

---
+++ templates/admin/dashboard.php
@@ -0,0 +1,30 @@
+<div class="wrap">
+
+    <h1>Great Marketrealm Companion</h1>
+
+    <div class="gmrc-card">
+
+        <h2>Plugin Status</h2>
+
+        <table class="widefat">
+
+            <tr>
+                <th>Plugin Version</th>
+                <td><?php echo esc_html($stats['plugin_version']); ?></td>
+            </tr>
+
+            <tr>
+                <th>Database Version</th>
+                <td><?php echo esc_html($stats['database_version']); ?></td>
+            </tr>
+
+            <tr>
+                <th>Total Characters</th>
+                <td><?php echo esc_html($stats['characters']); ?></td>
+            </tr>
+
+        </table>
+
+    </div>
+
+</div>
