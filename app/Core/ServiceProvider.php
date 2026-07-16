0fca56f31c0975465ac206c7b3545347268bf6c7
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-16 15:08:17 +0100

    Update ServiceProvider.php

--- app/Core/ServiceProvider.php
+++ app/Core/ServiceProvider.php
@@ -1,2 +1,51 @@
 <?php
-// silence is golden
+
+namespace GreatMarketrealmCompanion\Core;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Base Service Provider.
+ *
+ * @package GreatMarketrealmCompanion
+ * @since 0.2.0-alpha3.2
+ */
+abstract class ServiceProvider
+{
+    /**
+     * The application instance.
+     */
+    protected Application $app;
+
+    /**
+     * Constructor.
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Register bindings.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Boot services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    /**
+     * Get the application instance.
+     */
+    public function app(): Application
+    {
+        return $this->app;
+    }
+}
