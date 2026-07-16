b1202a5d098c02581c64085d088bf1b97d583ded
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-16 13:17:28 +0100

    Update app.php

--- resources/views/layouts/app.php
+++ resources/views/layouts/app.php
@@ -1,2 +1,66 @@
-<?php 
-// silence is golden
+<?php
+
+defined('ABSPATH') || exit;
+
+use GreatMarketrealmCompanion\View\View;
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+
+    <meta charset="<?php bloginfo('charset'); ?>">
+
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1">
+
+    <title>
+
+        <?php
+
+        echo esc_html(
+            $page_title ?? get_bloginfo('name')
+        );
+
+        ?>
+
+    </title>
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class('gmrc-app'); ?>>
+
+<?php wp_body_open(); ?>
+
+<div class="gmrc-app">
+
+    <?php View::component('header'); ?>
+
+    <div class="gmrc-container">
+
+        <?php View::component('sidebar'); ?>
+
+        <main class="gmrc-content">
+
+            <?php
+
+            echo $content;
+
+            ?>
+
+        </main>
+
+    </div>
+
+    <?php View::component('footer'); ?>
+
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
