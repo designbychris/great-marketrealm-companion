7751a59c992ff17f834a08a26913f85f7d8b0491
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-17 11:05:49 +0100

    Create ContainerException.php

---
+++ app/Exceptions/ContainerException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GreatMarketrealmCompanion\Exceptions;
+
+use Exception;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Thrown when the container cannot resolve a dependency.
+ *
+ * @package MarketrealmCompanion
+ * @since 0.2.0-alpha3.2
+ */
+class ContainerException extends Exception
+{
+}
