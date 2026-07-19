b9609fdc8d761afa28abfd17862af168c2707514
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-19 13:18:12 +0100

    Update CharacterRepository.php

--- app/Modules/Characters/Repositories/CharacterRepository.php
+++ app/Modules/Characters/Repositories/CharacterRepository.php
@@ -1,1 +1,65 @@
+<?php
 
+namespace GreatMarketrealmCompanion\Modules\Characters\Repositories;
+
+use GreatMarketrealmCompanion\Modules\Characters\Models\Character;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Character Repository.
+ *
+ * Handles persistence for Character domain models.
+ *
+ * @package MarketrealmCompanion
+ * @since 0.3.0
+ */
+class CharacterRepository
+{
+    /**
+     * Return all characters.
+     *
+     * @return Character[]
+     */
+    public function all(): array
+    {
+        // Placeholder until custom post type/data layer is implemented.
+        return [];
+    }
+
+    /**
+     * Find a character by ID.
+     */
+    public function find(int $id): ?Character
+    {
+        // Placeholder implementation.
+        return null;
+    }
+
+    /**
+     * Create a new character.
+     */
+    public function create(Character $character): Character
+    {
+        // Persistence will be added later.
+        return $character;
+    }
+
+    /**
+     * Update an existing character.
+     */
+    public function update(Character $character): Character
+    {
+        // Persistence will be added later.
+        return $character;
+    }
+
+    /**
+     * Delete a character.
+     */
+    public function delete(Character $character): bool
+    {
+        // Persistence will be added later.
+        return true;
+    }
+}
