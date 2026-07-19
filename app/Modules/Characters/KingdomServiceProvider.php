0f88019c0d308e1372317dc2d26ca1191834e633
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-19 13:14:23 +0100

    Update KingdomServiceProvider.php

--- app/Modules/Characters/KingdomServiceProvider.php
+++ app/Modules/Characters/KingdomServiceProvider.php
@@ -1,1 +1,34 @@
+<?php
 
+namespace GreatMarketrealmCompanion\Modules\Characters;
+
+use GreatMarketrealmCompanion\Providers\ServiceProvider;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Characters Kingdom Service Provider.
+ *
+ * Registers the Characters Kingdom.
+ *
+ * @package MarketrealmCompanion
+ * @since 0.3.0
+ */
+class KingdomServiceProvider extends ServiceProvider
+{
+    /**
+     * Register kingdom services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Boot the kingdom.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
