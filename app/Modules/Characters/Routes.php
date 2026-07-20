3c8198be7a630f06ca8e95bd772a773067e58f6a
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-20 09:30:45 +0100

    Update Routes.php

--- app/Modules/Characters/Routes.php
+++ app/Modules/Characters/Routes.php
@@ -1,1 +1,42 @@
+<?php
 
+use GreatMarketrealmCompanion\Modules\Characters\Controllers\CharacterController;
+
+return static function ($router): void {
+
+    $router->get(
+        '/characters',
+        [CharacterController::class, 'index']
+    );
+
+    $router->get(
+        '/characters/create',
+        [CharacterController::class, 'create']
+    );
+
+    $router->post(
+        '/characters',
+        [CharacterController::class, 'store']
+    );
+
+    $router->get(
+        '/characters/{id}',
+        [CharacterController::class, 'show']
+    );
+
+    $router->get(
+        '/characters/{id}/edit',
+        [CharacterController::class, 'edit']
+    );
+
+    $router->put(
+        '/characters/{id}',
+        [CharacterController::class, 'update']
+    );
+
+    $router->delete(
+        '/characters/{id}',
+        [CharacterController::class, 'destroy']
+    );
+
+};
