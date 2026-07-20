1855ea3bbbbc5eb8b6eef5ec14d114c40fb663d3
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-20 14:37:18 +0100

    Update View.php

--- app/Core/View/View.php
+++ app/Core/View/View.php
@@ -1,7 +1,68 @@
 <?php
-View::make(
-    'characters.index',
-    [
-        'characters' => $characters,
-    ]
-);
+
+namespace GreatMarketrealmCompanion\Core\View;
+
+defined('ABSPATH') || exit;
+
+/**
+ * View.
+ *
+ * Represents a renderable application view.
+ *
+ * @package GreatMarketrealmCompanion
+ * @since 0.3.0
+ */
+class View
+{
+    /**
+     * View name.
+     */
+    protected string $name;
+
+    /**
+     * View data.
+     *
+     * @var array<string, mixed>
+     */
+    protected array $data = [];
+
+    /**
+     * Constructor.
+     */
+    protected function __construct(
+        string $name,
+        array $data = []
+    ) {
+        $this->name = $name;
+        $this->data = $data;
+    }
+
+    /**
+     * Create a new view instance.
+     */
+    public static function make(
+        string $name,
+        array $data = []
+    ): self {
+        return new self(
+            $name,
+            $data
+        );
+    }
+
+    /**
+     * Return the view name.
+     */
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Return the view data.
+     */
+    public function data(): array
+    {
+        return $this->data;
+    }
+}
