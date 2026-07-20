89cbdefc7c2d8009b678207d37c13fd29af9f9a8
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-20 09:27:15 +0100

    Update CharacterController.php

--- app/Modules/Characters/Controllers/CharacterController.php
+++ app/Modules/Characters/Controllers/CharacterController.php
@@ -1,1 +1,35 @@
+<?php
 
+namespace GreatMarketrealmCompanion\Modules\Characters\Controllers;
+
+use GreatMarketrealmCompanion\Http\Page;
+use GreatMarketrealmCompanion\Modules\Characters\Repositories\CharacterRepository;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Character Controller.
+ *
+ * Handles requests for the Characters Kingdom.
+ *
+ * @package MarketrealmCompanion
+ * @since 0.3.0
+ */
+class CharacterController
+{
+    public function __construct(
+        protected CharacterRepository $repository
+    ) {
+    }
+
+    public function index(): string
+    {
+        return Page::make('characters.index')
+            ->title(__('Characters', 'great-marketrealm-companion'))
+            ->layout('app')
+            ->with([
+                'characters' => $this->repository->all(),
+            ])
+            ->render();
+    }
+}
