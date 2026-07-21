c23369acfbfd9d111233555509e3b324681773f0
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-21 18:36:06 +0100

    Update PermissionManager.php

--- app/Permissions/PermissionManager.php
+++ app/Permissions/PermissionManager.php
@@ -1,3 +1,37 @@
-<?php 
+<?php
 
-namespace GreatMarketrealmCompanion\Permissions;
+namespace GreatMarketrealmCompanion\Providers;
+
+use GreatMarketrealmCompanion\Navigation\Navigation;
+use GreatMarketrealmCompanion\Permissions\PermissionManager;
+
+defined('ABSPATH') || exit;
+
+class NavigationServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->app->singleton(
+            PermissionManager::class,
+            static function (): PermissionManager {
+                return new PermissionManager();
+            }
+        );
+
+        $this->app->singleton(
+            Navigation::class,
+            function (): Navigation {
+                return new Navigation(
+                    $this->app->make(PermissionManager::class)
+                );
+            }
+        );
+    }
+
+    public function boot(): void
+    {
+        $navigation = $this->app->make(Navigation::class);
+
+        // Register navigation items here.
+    }
+}
