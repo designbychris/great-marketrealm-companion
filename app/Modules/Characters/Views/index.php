0b3f269e4726e1d921a412430e226cb41e7668a9
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-21 09:21:14 +0100

    Update index.php

--- app/Modules/Characters/Views/index.php
+++ app/Modules/Characters/Views/index.php
@@ -1,9 +1,45 @@
-<?php ?>
+<?php
 
-<h1>Characters Kingdom</h1>
+defined('ABSPATH') || exit;
 
-<p>Welcome to the Characters Kingdom!</p>
+?>
 
-<p>Characters loaded:
-    <?= count($characters); ?>
-</p>
+<section class="gmrc-page">
+
+    <header class="gmrc-page__header">
+
+        <h1>Characters Kingdom</h1>
+
+        <p>
+            Welcome to the Characters Kingdom.
+        </p>
+
+    </header>
+
+    <main class="gmrc-page__content">
+
+        <?php if (empty($characters)) : ?>
+
+            <p>
+                No characters have been created yet.
+            </p>
+
+        <?php else : ?>
+
+            <ul>
+
+                <?php foreach ($characters as $character) : ?>
+
+                    <li>
+                        <?= esc_html($character->name()); ?>
+                    </li>
+
+                <?php endforeach; ?>
+
+            </ul>
+
+        <?php endif; ?>
+
+    </main>
+
+</section>
