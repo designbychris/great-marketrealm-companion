df29f52627906c8f61089ec661ee699f43314102
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-21 08:58:06 +0100

    Update ViewFactory.php

--- app/Core/View/ViewFactory.php
+++ app/Core/View/ViewFactory.php
@@ -1,1 +1,53 @@
+<?php
 
+namespace GreatMarketrealmCompanion\Core\View;
+
+defined('ABSPATH') || exit;
+
+/**
+ * View Factory.
+ *
+ * Renders application views.
+ *
+ * @package MarketrealmCompanion
+ * @since 0.3.0
+ */
+class ViewFactory
+{
+    /**
+     * View finder.
+     */
+    protected ViewFinder $finder;
+
+    /**
+     * Constructor.
+     */
+    public function __construct(
+        ViewFinder $finder
+    ) {
+        $this->finder = $finder;
+    }
+
+    /**
+     * Render a view.
+     */
+    public function render(
+        View $view
+    ): string {
+
+        $path = $this->finder->find(
+            $view->name()
+        );
+
+        extract(
+            $view->data(),
+            EXTR_SKIP
+        );
+
+        ob_start();
+
+        require $path;
+
+        return ob_get_clean();
+    }
+}
