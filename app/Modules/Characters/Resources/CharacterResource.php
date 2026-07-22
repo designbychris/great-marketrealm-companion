9ba59de469315e4d4219b3c1fd7eae6ab4e947fc
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-22 13:55:08 +0100

    Create CharacterResource.php

---
+++ app/Modules/Characters/Resources/CharacterResource.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace GreatMarketrealmCompanion\Modules\Characters\Resources;
+
+use GreatMarketrealmCompanion\Modules\Characters\Controllers\CharacterController;
+use GreatMarketrealmCompanion\Resources\Resource;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Character Resource.
+ *
+ * Defines the application behaviour for managing characters.
+ *
+ * @package GreatMarketrealmCompanion
+ * @since 0.4.0
+ */
+class CharacterResource extends Resource
+{
+    /**
+     * Unique Resource key.
+     */
+    public function key(): string
+    {
+        return 'characters';
+    }
+
+    /**
+     * Singular display name.
+     */
+    public function singularName(): string
+    {
+        return 'Character';
+    }
+
+    /**
+     * Plural display name.
+     */
+    public function pluralName(): string
+    {
+        return 'Characters';
+    }
+
+    /**
+     * Base route prefix.
+     */
+    public function routePrefix(): string
+    {
+        return '/characters';
+    }
+
+    /**
+     * Resource controller.
+     */
+    public function controller(): string
+    {
+        return CharacterController::class;
+    }
+}
