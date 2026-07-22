26081c7fe32b70608582c529ce2e17a616fc1832
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-22 12:54:49 +0100

    Update Router.php

--- app/Core/Routing/Router.php
+++ app/Core/Routing/Router.php
@@ -1,11 +1,46 @@
 <?php
 
-namespace GreatMarketrealmCompanion\Core\Routing;
-
-use GreatMarketrealmCompanion\Core\Container;
-use RuntimeException;
+use GreatMarketrealmCompanion\Core\Routing\Router;
+use GreatMarketrealmCompanion\Modules\Characters\Controllers\CharacterController;
 
 defined('ABSPATH') || exit;
+
+return static function (Router $router): void {
+    $router->get(
+        '/characters',
+        [CharacterController::class, 'index']
+    );
+
+    $router->get(
+        '/characters/create',
+        [CharacterController::class, 'create']
+    );
+
+    $router->post(
+        '/characters',
+        [CharacterController::class, 'store']
+    );
+
+    $router->get(
+        '/characters/{id}',
+        [CharacterController::class, 'show']
+    );
+
+    $router->get(
+        '/characters/{id}/edit',
+        [CharacterController::class, 'edit']
+    );
+
+    $router->put(
+        '/characters/{id}',
+        [CharacterController::class, 'update']
+    );
+
+    $router->delete(
+        '/characters/{id}',
+        [CharacterController::class, 'destroy']
+    );
+};
 
 class Router
 {
