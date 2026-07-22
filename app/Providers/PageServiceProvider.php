2e11a73ed2e54539c797f707389d343fd3582e15
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-22 16:58:42 +0100

    Create PageServiceProvider.php

---
+++ app/Providers/PageServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace GreatMarketrealmCompanion\Providers;
+
+use GreatMarketrealmCompanion\Core\Pages\PageRegistry;
+use GreatMarketrealmCompanion\Foundation\ServiceProvider;
+use GreatMarketrealmCompanion\Resources\ResourceRegistry;
+
+defined('ABSPATH') || exit;
+
+class PageServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->app->singleton(
+            PageRegistry::class,
+            static fn (): PageRegistry => new PageRegistry()
+        );
+    }
+
+    public function boot(): void
+    {
+        $pages = $this->app->make(
+            PageRegistry::class
+        );
+
+        $resources = $this->app->make(
+            ResourceRegistry::class
+        );
+
+        foreach ($resources->all() as $resource) {
+            $pages->registerResource($resource);
+        }
+    }
+}
