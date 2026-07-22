7284bc7b667deee77a6b67e1e7d5497509030edb
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-22 13:56:23 +0100

    Create ResourceServiceProvider.php

---
+++ app/Providers/ResourceServiceProvider.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace GreatMarketrealmCompanion\Providers;
+
+use GreatMarketrealmCompanion\Core\Routing\Router;
+use GreatMarketrealmCompanion\Kingdoms\KingdomRegistry;
+use GreatMarketrealmCompanion\Resources\Resource;
+use GreatMarketrealmCompanion\Resources\ResourceRegistry;
+use RuntimeException;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Resource Service Provider.
+ *
+ * Discovers and registers Resources contributed
+ * by application Kingdoms.
+ *
+ * @package GreatMarketrealmCompanion
+ * @since 0.4.0
+ */
+class ResourceServiceProvider extends ServiceProvider
+{
+    /**
+     * Register Resource services.
+     */
+    public function register(): void
+    {
+        $this->app->container()->singleton(
+            ResourceRegistry::class,
+            static fn (): ResourceRegistry => new ResourceRegistry()
+        );
+    }
+
+    /**
+     * Register Kingdom Resources and their routes.
+     */
+    public function boot(): void
+    {
+        $resources = $this->app->make(
+            ResourceRegistry::class
+        );
+
+        $kingdoms = $this->app->make(
+            KingdomRegistry::class
+        );
+
+        foreach ($kingdoms->resourceClasses() as $resourceClass) {
+            $resources->add(
+                $this->createResource(
+                    $resourceClass
+                )
+            );
+        }
+
+        $router = $this->app->make(
+            Router::class
+        );
+
+        $resources->registerRoutes(
+            $router
+        );
+
+        do_action(
+            'gmrc_resources_registered',
+            $resources
+        );
+    }
+
+    /**
+     * Create a Resource instance.
+     *
+     * @param class-string<Resource> $resourceClass
+     */
+    protected function createResource(
+        string $resourceClass
+    ): Resource {
+        if (! is_subclass_of(
+            $resourceClass,
+            Resource::class
+        )) {
+            throw new RuntimeException(
+                sprintf(
+                    'Invalid Resource class: %s',
+                    $resourceClass
+                )
+            );
+        }
+
+        return new $resourceClass(
+            $this->app
+        );
+    }
+}
