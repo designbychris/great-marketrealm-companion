cd7c788351f817fe33b0190bd34a3ce76d77627c
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-23 14:19:01 +0100

    Update Routes.php

--- app/Modules/Characters/Routes.php
+++ app/Modules/Characters/Routes.php
@@ -6,24 +6,10 @@ use GreatMarketrealmCompanion\Modules\Characters\Controllers\CharacterController
 defined('ABSPATH') || exit;
 
 return static function (Router $router): void {
-    $router->get(
-        '/characters/create',
-        [CharacterController::class, 'create']
-    );
 
     $router->post(
         '/characters',
         [CharacterController::class, 'store']
-    );
-
-    $router->get(
-        '/characters/{id}',
-        [CharacterController::class, 'show']
-    );
-
-    $router->get(
-        '/characters/{id}/edit',
-        [CharacterController::class, 'edit']
     );
 
     $router->put(
