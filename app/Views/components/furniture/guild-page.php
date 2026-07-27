68b9f6c668c267f1bf5980be3c3e77d60103af7e
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-27 14:38:23 +0100

    Update guild-page.php

--- app/Views/components/furniture/guild-page.php
+++ app/Views/components/furniture/guild-page.php
@@ -3,17 +3,36 @@
  * Guild page component.
  *
  * Variables:
- * $side    string left, right or single.
+ *
+ * $side    string Left, right or single.
  * $content string Rendered page content.
  * $class   string Optional additional classes.
+ * $spine   bool   Whether the leather spine should be displayed.
  */
 
 defined('ABSPATH') || exit;
 
-$side = isset($side) ? sanitize_html_class($side) : 'single';
-$class = isset($class) ? $class : '';
+$content = isset($content)
+	? (string) $content
+	: '';
 
-$allowed_sides = array('left', 'right', 'single');
+$side = isset($side)
+	? sanitize_key((string) $side)
+	: 'single';
+
+$class = isset($class)
+	? trim((string) $class)
+	: '';
+
+$spine = isset($spine)
+	? (bool) $spine
+	: true;
+
+$allowed_sides = array(
+	'left',
+	'right',
+	'single',
+);
 
 if (!in_array($side, $allowed_sides, true)) {
 	$side = 'single';
@@ -24,35 +43,50 @@ $classes = array(
 	'guild-page--' . $side,
 );
 
-if (!empty($class)) {
+if ($spine) {
+	$classes[] = 'guild-page--has-spine';
+
+	$classes[] = $side === 'right'
+		? 'guild-page--spine-right'
+		: 'guild-page--spine-left';
+}
+
+if ($class !== '') {
 	$additional_classes = preg_split('/\s+/', $class);
 
 	foreach ($additional_classes as $additional_class) {
-		$classes[] = sanitize_html_class($additional_class);
+		$additional_class = sanitize_html_class($additional_class);
+
+		if ($additional_class !== '') {
+			$classes[] = $additional_class;
+		}
 	}
 }
 ?>
 
-<section class="<?php echo esc_attr(implode(' ', array_filter($classes))); ?>">
+<section class="<?php echo esc_attr(implode(' ', $classes)); ?>">
 
-    <?php if ($spine) : ?>
+	<?php if ($spine) : ?>
 
-        <?php
-        $position = 'left';
+		<?php
+		(function () use ($side): void {
+			$position = $side === 'right'
+				? 'right'
+				: 'left';
 
-        require GMRC_PATH
-            . 'app/Views/furniture/ledger-spine.php';
-        ?>
+			require GMRC_PATH
+				. 'app/Views/components/furniture/ledger-spine.php';
+		})();
+		?>
 
-    <?php endif; ?>
+	<?php endif; ?>
 
-    <div class="guild-page__content">
-
-        <?php
-        echo $content;
-        // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-        ?>
-
-    </div>
+	<div class="guild-page__content">
+		<?php
+		// The content has already been rendered by a trusted view.
+		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo $content;
+		?>
+	</div>
 
 </section>
