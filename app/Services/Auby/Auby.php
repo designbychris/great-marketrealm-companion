648bac66b37b1e5263ef1290ca8dbfa945e7cd06
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-27 16:01:20 +0100

    Update Auby.php

--- app/Services/Auby/Auby.php
+++ app/Services/Auby/Auby.php
@@ -1,1 +1,28 @@
+<?php
 
+declare(strict_types=1);
+
+namespace GMRC\Services\Auby;
+
+final class Auby
+{
+    private QuoteRepository $quotes;
+
+    public function __construct(
+        ?QuoteRepository $quotes = null
+    ) {
+        $this->quotes = $quotes ?? new QuoteRepository();
+    }
+
+    public function note(
+        string $category = QuoteCategories::GENERAL
+    ): Quote {
+        return $this->quotes->random($category);
+    }
+
+    public function for(
+        string $category = QuoteCategories::GENERAL
+    ): Quote {
+        return $this->note($category);
+    }
+}
