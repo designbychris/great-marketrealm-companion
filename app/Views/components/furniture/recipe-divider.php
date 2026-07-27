60f18d5bb2484368d0c4df796000e45adf0f5e77
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-27 13:56:40 +0100

    Update recipe-divider.php

--- app/Views/components/furniture/recipe-divider.php
+++ app/Views/components/furniture/recipe-divider.php
@@ -1,1 +1,18 @@
+<?php
+/**
+ * Decorative recipe divider.
+ *
+ * Variables:
+ * $ornament string Optional decorative character.
+ */
 
+defined('ABSPATH') || exit;
+
+$ornament = isset($ornament) ? $ornament : '✦';
+?>
+
+<div class="recipe-divider" aria-hidden="true">
+	<span class="recipe-divider__ornament">
+		<?php echo esc_html($ornament); ?>
+	</span>
+</div>
