c3ff7505f4989b31e2dc3f9e4c0b31ba33cee29f
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-27 16:57:27 +0100

    Update ViewFinder.php

--- app/Core/View/ViewFinder.php
+++ app/Core/View/ViewFinder.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GreatMarketrealmCompanion\Core\View;
 
 defined('ABSPATH') || exit;
@@ -7,7 +9,7 @@ defined('ABSPATH') || exit;
 /**
  * View Finder.
  *
- * Locates application view files.
+ * Locates module views and shared application views.
  *
  * @package MarketrealmCompanion
  * @since 0.3.0
@@ -17,11 +19,74 @@ class ViewFinder
     /**
      * Find a view.
      *
+     * Module view example:
+     *
+     * characters.index
+     *
+     * Resolves to:
+     *
+     * app/Modules/Characters/Views/index.php
+     *
+     * Shared view example:
+     *
+     * components.furniture.auby-note
+     *
+     * Resolves to:
+     *
+     * app/Views/components/furniture/auby-note.php
+     *
      * @throws \RuntimeException
      */
-    public function find(
-        string $view
-    ): string {
+    public function find(string $view): string
+    {
+        $sharedPath = $this->sharedViewPath($view);
+
+        if (file_exists($sharedPath)) {
+            return $sharedPath;
+        }
+
+        $modulePath = $this->moduleViewPath($view);
+
+        if ($modulePath !== null && file_exists($modulePath)) {
+            return $modulePath;
+        }
+
+        throw new \RuntimeException(
+            sprintf(
+                'View [%s] not found. Checked [%s]%s.',
+                $view,
+                $sharedPath,
+                $modulePath !== null
+                    ? sprintf(' and [%s]', $modulePath)
+                    : ''
+            )
+        );
+    }
+
+    /**
+     * Build a shared application view path.
+     */
+    protected function sharedViewPath(string $view): string
+    {
+        return sprintf(
+            '%sapp/Views/%s.php',
+            GMRC_PATH,
+            str_replace(
+                '.',
+                DIRECTORY_SEPARATOR,
+                $view
+            )
+        );
+    }
+
+    /**
+     * Build a module view path.
+     */
+    protected function moduleViewPath(string $view): ?string
+    {
+        if (! str_contains($view, '.')) {
+            return null;
+        }
 
         [$module, $template] = explode(
             '.',
@@ -29,22 +94,15 @@ class ViewFinder
             2
         );
 
-        $path = sprintf(
+        return sprintf(
             '%sapp/Modules/%s/Views/%s.php',
             GMRC_PATH,
             ucfirst($module),
-            $template
+            str_replace(
+                '.',
+                DIRECTORY_SEPARATOR,
+                $template
+            )
         );
-
-        if (! file_exists($path)) {
-            throw new \RuntimeException(
-                sprintf(
-                    'View [%s] not found.',
-                    $view
-                )
-            );
-        }
-
-        return $path;
     }
 }
