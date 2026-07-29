982df0e77d9949905e5be783ddbed3a46ce739d4
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-29 16:26:02 +0100

    Update DefinitionServiceProvider.php

--- app/Providers/DefinitionServiceProvider.php
+++ app/Providers/DefinitionServiceProvider.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace GreatMarketrealmCompanion\Providers;
 
-use GreatMarketrealmCompanion\Services\Definitions\DefinitionFactory;
+use GreatMarketrealmCompanion\Services\Definitions\Definitions;
 
 final class DefinitionServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->container()->singleton(
-            DefinitionFactory::class,
-            fn () => new DefinitionFactory(),
-        );
+        $this->app
+            ->container()
+            ->singleton(
+                Definitions::class,
+                static fn (): Definitions => new Definitions(),
+            );
     }
 
     public function boot(): void
