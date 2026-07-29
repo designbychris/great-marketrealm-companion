a2545e6f59bc7c45805f5378ad94c77278c5924d
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-29 16:36:06 +0100

    Update DefinitionServiceProvider.php

--- app/Providers/DefinitionServiceProvider.php
+++ app/Providers/DefinitionServiceProvider.php
@@ -6,18 +6,31 @@ namespace GreatMarketrealmCompanion\Providers;
 
 use GreatMarketrealmCompanion\Services\Definitions\Definitions;
 
+defined('ABSPATH') || exit;
+
+/**
+ * Definition Service Provider.
+ *
+ * Registers services belonging to the Definition domain.
+ *
+ * @since 0.3.0
+ */
 final class DefinitionServiceProvider extends ServiceProvider
 {
+    /**
+     * Register Definition services.
+     */
     public function register(): void
     {
-        $this->app
-            ->container()
-            ->singleton(
-                Definitions::class,
-                static fn (): Definitions => new Definitions(),
-            );
+        $this->app->singleton(
+            Definitions::class,
+            static fn (): Definitions => new Definitions()
+        );
     }
 
+    /**
+     * Boot Definition services.
+     */
     public function boot(): void
     {
     }
