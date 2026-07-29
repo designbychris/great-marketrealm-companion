10c98782f28f1bb6ed86e0c19d100c9f6a4907dd
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-29 11:30:46 +0100

    Update GuildServiceProvider.php

--- app/Providers/GuildServiceProvider.php
+++ app/Providers/GuildServiceProvider.php
@@ -1,14 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GreatMarketrealmCompanion\Providers;
 
 use GreatMarketrealmCompanion\Services\Guild\GuildSealRegistry;
 
-class GuildServiceProvider extends ServiceProvider
+defined('ABSPATH') || exit;
+
+/**
+ * Guild Service Provider.
+ *
+ * Registers services belonging to the Guild domain.
+ *
+ * @since 0.3.0
+ */
+final class GuildServiceProvider extends ServiceProvider
 {
+    /**
+     * Register Guild services.
+     */
     public function register(): void
     {
-        $this->app->container()->bind(
+        $this->app->container()->singleton(
             GuildSealRegistry::class,
             static function (): GuildSealRegistry {
                 return new GuildSealRegistry();
@@ -16,6 +30,9 @@ class GuildServiceProvider extends ServiceProvider
         );
     }
 
+    /**
+     * Boot Guild services.
+     */
     public function boot(): void
     {
     }
