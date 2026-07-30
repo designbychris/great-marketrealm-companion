0201c4b0f37a74137c57ec8af41b90a58b0b80bd
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-30 15:38:14 +0100

    Update RouterTest.php

--- tests/Unit/Router/RouterTest.php
+++ tests/Unit/Router/RouterTest.php
@@ -1,1 +1,46 @@
+<?php
 
+declare(strict_types=1);
+
+namespace GreatMarketrealmCompanion\Tests\Unit\Router;
+
+use GreatMarketrealmCompanion\Core\Container;
+use GreatMarketrealmCompanion\Core\Http\Request;
+use GreatMarketrealmCompanion\Core\Routing\Router;
+use PHPUnit\Framework\TestCase;
+
+final class RouterTest extends TestCase
+{
+    public function testRouterCanBeCreated(): void
+    {
+        $router = new Router(
+            new Container(),
+            new Request()
+        );
+
+        $this->assertInstanceOf(
+            Router::class,
+            $router
+        );
+    }
+
+    public function testRouterRegistersGetRoute(): void
+    {
+        $router = new Router(
+            new Container(),
+            new Request()
+        );
+
+        $router->get(
+            '/characters',
+            static fn (): string => 'characters'
+        );
+
+        $this->assertTrue(
+            $router->has(
+                'GET',
+                '/characters'
+            )
+        );
+    }
+}
