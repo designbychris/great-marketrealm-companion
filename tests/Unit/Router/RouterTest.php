efb0ee7707fe4dfc24247ee5568c761f39f0126e
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-30 16:27:53 +0100

    Update RouterTest.php

--- tests/Unit/Router/RouterTest.php
+++ tests/Unit/Router/RouterTest.php
@@ -7,7 +7,6 @@ namespace GreatMarketrealmCompanion\Tests\Unit\Router;
 use GreatMarketrealmCompanion\Core\Container;
 use GreatMarketrealmCompanion\Core\Http\Request;
 use GreatMarketrealmCompanion\Core\Routing\Router;
-use GreatMarketrealmCompanion\Core\Http\Response;
 use PHPUnit\Framework\TestCase;
 
 final class RouterTest extends TestCase
