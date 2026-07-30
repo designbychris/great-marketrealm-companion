b68c3ccec0ce4ec56d5f9cd86b4157973b8b94b6
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-30 13:25:57 +0100

    Update Routes.php

--- app/Modules/Characters/Routes.php
+++ app/Modules/Characters/Routes.php
@@ -7,8 +7,6 @@ defined('ABSPATH') || exit;
 
 return static function (Router $router): void {
 
-    error_log('Characters Routes.php registrar executed');
-
     $router->get(
         '/characters',
         [CharacterController::class, 'index']
