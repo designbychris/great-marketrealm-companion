7aa26875ba7269337f64efb8a448f8d2cdd60ca2
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-30 14:37:05 +0100

    Update ContainerTest.php

--- tests/Unit/Container/ContainerTest.php
+++ tests/Unit/Container/ContainerTest.php
@@ -1,1 +1,21 @@
+<?php
 
+declare(strict_types=1);
+
+namespace GreatMarketrealmCompanion\Tests\Unit;
+
+use GreatMarketrealmCompanion\Core\Container;
+use PHPUnit\Framework\TestCase;
+
+final class ContainerTest extends TestCase
+{
+    public function testContainerCanBeCreated(): void
+    {
+        $container = new Container();
+
+        $this->assertInstanceOf(
+            Container::class,
+            $container
+        );
+    }
+}
