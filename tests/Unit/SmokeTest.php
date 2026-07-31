196a7b1078d61db68c71f8158755e6307cab0807
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-31 14:23:10 +0100

    Update SmokeTest.php

--- tests/Unit/SmokeTest.php
+++ tests/Unit/SmokeTest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 namespace GreatMarketrealmCompanion\Tests\Unit;
-use GreatMarketrealmCompanion\Tests\Stubs\DispatchingRouterSpy;
+
 use PHPUnit\Framework\TestCase;
 
 final class EmptyRouterSpy
@@ -19,7 +19,9 @@ final class SmokeTest extends TestCase
     public function test_can_create_router_spy(): void
     {
         fwrite(STDERR, "1\n");
-
+        
+        use GreatMarketrealmCompanion\Tests\Stubs\DispatchingRouterSpy;
+        
         $router = new DispatchingRouterSpy();
 
         fwrite(STDERR, "2\n");
