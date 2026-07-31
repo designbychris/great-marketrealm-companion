4274b765f395b1babb169f39fb91fe7418755a06
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-31 11:56:51 +0100

    Create RouteResolver.php

---
+++ app/Core/Http/RouteResolver.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GreatMarketrealmCompanion\Core\Http;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Resolves the current Companion route.
+ *
+ * @package GreatMarketrealmCompanion
+ * @since 0.4.0
+ */
+final class RouteResolver
+{
+    /**
+     * Resolve the requested route.
+     */
+    public function current(): string
+    {
+        if (isset($_POST['gmrc_route'])) {
+            $route = sanitize_text_field(
+                wp_unslash($_POST['gmrc_route'])
+            );
+        } elseif (isset($_GET['gmrc_route'])) {
+            $route = sanitize_text_field(
+                wp_unslash($_GET['gmrc_route'])
+            );
+        } else {
+            $route = 'dashboard';
+        }
+
+        $route = preg_replace(
+            '#[^a-zA-Z0-9/_-]#',
+            '',
+            $route
+        );
+
+        $route = trim(
+            is_string($route)
+                ? $route
+                : '',
+            '/'
+        );
+
+        return $route !== ''
+            ? $route
+            : 'dashboard';
+    }
+}
