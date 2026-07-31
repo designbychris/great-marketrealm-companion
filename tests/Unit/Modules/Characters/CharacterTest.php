c38d42c810becd9c85a779d4634b60c15c9efb79
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-31 16:20:42 +0100

    Create CharacterTest.php

---
+++ tests/Unit/Modules/Characters/CharacterTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Modules\Characters\Models;
+
+use GreatMarketrealmCompanion\Modules\Characters\Models\Character;
+use PHPUnit\Framework\TestCase;
+
+final class CharacterTest extends TestCase
+{
+}
