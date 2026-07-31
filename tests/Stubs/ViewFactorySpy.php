dbeb0601be2e971d9936c594e21326195cd5a480
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-31 13:00:50 +0100

    Update ViewFactorySpy.php

--- tests/Stubs/ViewFactorySpy.php
+++ tests/Stubs/ViewFactorySpy.php
@@ -7,19 +7,57 @@ namespace GreatMarketrealmCompanion\Tests\Stubs;
 use GreatMarketrealmCompanion\Core\View\View;
 use GreatMarketrealmCompanion\Core\View\ViewFactory;
 
+/**
+ * Test view factory that records rendered views.
+ */
 final class ViewFactorySpy extends ViewFactory
 {
-    public ?View $view = null;
+    /**
+     * Recorded views.
+     *
+     * @var array<int, View>
+     */
+    private array $views = [];
 
+    /**
+     * The spy does not require ViewFactory's production dependencies.
+     */
     public function __construct()
     {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function render(
         View $view
     ): string {
-        $this->view = $view;
+        $this->views[] = $view;
 
         return '<view />';
+    }
+
+    /**
+     * Return every rendered view.
+     *
+     * @return array<int, View>
+     */
+    public function views(): array
+    {
+        return $this->views;
+    }
+
+    /**
+     * Return the last rendered view.
+     */
+    public function lastView(): ?View
+    {
+        if ($this->views === []) {
+            return null;
+        }
+
+        return $this->views[
+            array_key_last($this->views)
+        ];
     }
 }
