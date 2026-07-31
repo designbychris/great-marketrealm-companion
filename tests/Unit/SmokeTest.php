d25c876569cf5708c241429e37fdb0debaf1c9df
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-31 14:29:23 +0100

    Update SmokeTest.php

--- tests/Unit/SmokeTest.php
+++ tests/Unit/SmokeTest.php
@@ -11,17 +11,11 @@ final class SmokeTest extends TestCase
 {
     public function test_can_create_router_spy(): void
     {
-        fwrite(STDERR, "1\n");
-
         $router = new DispatchingRouterSpy();
-
-        fwrite(STDERR, "2\n");
 
         self::assertInstanceOf(
             DispatchingRouterSpy::class,
             $router
         );
-
-        fwrite(STDERR, "3\n");
     }
 }
