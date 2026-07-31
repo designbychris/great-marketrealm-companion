87a548a08e26fb3d9dc1afb8dbceb1c20fe3a3cf
Christopher Mitchell <chris@designbychris.co.uk>
2026-07-31 10:22:30 +0100

    Create DummyObject.php

---
+++ tests/Stubs/DummyObject.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GreatMarketrealmCompanion\Tests\Stubs;
+
+final class DummyObject
+{
+    public function __construct(
+        public mixed $value = null
+    ) {
+    }
+}
