c46eb51626d33c258264e33824b6011cbc20db33
Christopher Mitchell <chris@designbychris.co.uk>
2026-08-05 16:17:12 +0100

    Update CharacterControllerTest.php

--- tests/Unit/Modules/Characters/Controllers/CharacterControllerTest.php
+++ tests/Unit/Modules/Characters/Controllers/CharacterControllerTest.php
@@ -73,6 +73,7 @@ namespace GreatMarketrealmCompanion\Tests\Unit\Modules\Characters\Controllers {
     use GreatMarketrealmCompanion\Modules\Characters\Portraits\Services\PortraitRecipeGenerator;
     use GreatMarketrealmCompanion\Modules\Characters\Portraits\Services\PortraitRenderer;
     use GreatMarketrealmCompanion\Modules\Characters\Portraits\Models\CharacterPortrait;
+    use GreatMarketrealmCompanion\Modules\Characters\Portraits\Contracts\PortraitLayerRegistryInterface;
     use GreatMarketrealmCompanion\Services\Auby\Auby;
     use GreatMarketrealmCompanion\Services\Auby\Quote;
     use GreatMarketrealmCompanion\Services\Auby\QuoteCategories;
