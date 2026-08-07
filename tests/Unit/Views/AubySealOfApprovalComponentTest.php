b108ad6e365685fb310f788849faf2e6fe2a3888
Christopher Mitchell <chris@designbychris.co.uk>
2026-08-07 12:41:39 +0100

    Add files via upload

---
+++ tests/Unit/Views/AubySealOfApprovalComponentTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GreatMarketrealmCompanion\Tests\Unit\Views;
+
+use PHPUnit\Framework\TestCase;
+
+final class AubySealOfApprovalComponentTest extends TestCase
+{
+    public function testSealAssetsExist(): void
+    {
+        $root = dirname(__DIR__, 2);
+
+        foreach (
+            [
+                'seal-of-approval.svg',
+                'seal-ink-splatter.svg',
+                'seal-of-approval-one-colour.svg',
+                'seal-of-approval-embossed.svg',
+                'seal-of-approval-gold.svg',
+            ] as $asset
+        ) {
+            self::assertFileExists(
+                $root
+                . '/assets/images/auby/seals/'
+                . $asset
+            );
+        }
+    }
+
+    public function testReusableSealComponentExists(): void
+    {
+        $root = dirname(__DIR__, 2);
+
+        self::assertFileExists(
+            $root
+            . '/app/Views/components/auby/'
+            . 'seal-of-approval.php'
+        );
+
+        self::assertFileExists(
+            $root
+            . '/assets/css/components/auby/'
+            . 'seal-of-approval.css'
+        );
+
+        self::assertFileExists(
+            $root
+            . '/assets/js/components/auby/'
+            . 'seal-of-approval.js'
+        );
+    }
+}
