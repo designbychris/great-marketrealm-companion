974353bcd675a66e7f7a7a3e5d46df40556c9992
Christopher Mitchell <chris@designbychris.co.uk>
2026-08-07 14:19:17 +0100

    Add files via upload

---
+++ tests/Unit/Views/AubySealApprovalBaselineTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GreatMarketrealmCompanion\Tests\Unit\Views;
+
+use PHPUnit\Framework\TestCase;
+
+final class AubySealApprovalBaselineTest extends TestCase
+{
+    public function testApprovalArcIsLowerThanPreviousBaseline(): void
+    {
+        $root = dirname(__DIR__, 3);
+
+        $seal = file_get_contents(
+            $root
+            . '/assets/images/auby/seals/'
+            . 'seal-of-approval.svg'
+        );
+
+        self::assertIsString($seal);
+
+        self::assertStringContainsString(
+            'd="M59 200',
+            $seal
+        );
+
+        self::assertStringContainsString(
+            'A106 106 0 0 0 261 200',
+            $seal
+        );
+
+        self::assertStringContainsString(
+            '>APPROVAL</textPath>',
+            $seal
+        );
+    }
+}
